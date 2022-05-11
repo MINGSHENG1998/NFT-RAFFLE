6c03da16cc4048a4cc6c90b485aa2dae7f2e8df6
Ming Sheng Khaw <xxxmingshengxxx@gmail.com>
2022-05-11 21:58:45 +0800

    added temp chart

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
@@ -15,7 +17,10 @@ const Home = () => {
           <Widget type="user"/>
           <Widget type="driver"/>
         </div>
-        <div className="charts"></div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );
